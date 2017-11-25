29c42cc35dff5818bce13adf94c82130c668554a
“BabaSar” <Baba.Sariffodeen.1@gmail.com>
2017-11-25 18:54:44 +0000

    before moving to another pc

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-console.log("Starting app.js");
+//console.log("Starting app.js");
 
-const fs = require("fs");
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require("./notes.js");
+
+var command = process.argv[2];
+
+if(command === 'add'){
+    console.log("adding new note");
+}else if (command === 'list'){
+    console.log("listing all notes");
+}else if (command === 'read'){
+    console.log("reading note");
+}else if (command === 'remove'){
+    console.log("removing note");
+}else{
+    console.log("command not recognized");
+};
+
 
 
