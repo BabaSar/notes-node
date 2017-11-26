254d87617f08b496bc1f5abca496d150e46d6fe6
BabaSar <Baba.Sariffodeen.1@gmail.com>
2017-11-26 13:06:36 +0000

    update

--- notes.js
+++ notes.js
@@ -8,13 +8,37 @@ var addNote = (title, body) => {
       body
     };
 
-    //push into array
-    notes.push(note);
+    try{
+        //fetch the existing notes
+        //check is title already exists (we don't want duplicates)
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
 
-    //write JSON string to file
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        //loop through notes collection and check for duplicate
+        for (var i=0; i<notes.length; i++)
+        {
+            if(notes[i].title === title){
+                console.log("Duplicate detected!");
+                break;
+            }else{
+                //push into array
+                notes.push(note);
+                //write JSON string to file
+                fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+            }
 
-};
+            //console.log(notes[i].title);
+        }
+    }catch(e){
+        console.log("Could not fetch from package.json file!");
+    }
+
+
+
+
+
+
+}
 
 var getAll = () => {
     console.log("Getting all notes");
